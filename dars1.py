dec9173f947f25c37de3613e7c8c7128dc405737
azizbek-312 <gullolaiskandarova36@gmail.com>
2024-06-12 15:57:37 +0500

    Create dars1.py

---
+++ dars1.py
@@ -0,0 +1,7 @@
+ismlar = ['zilola', 'shahnoza', ' gullola']
+duganam = "assalomu aleykum, " + ismlar[0].title() + "!"
+print(duganam)
+duganam = "Yaxshimjsan, " + ismlar[1].title() + "!"
+print(duganam)
+duganam = "nima gaplar, " + ismlar[2].title() + "!"
+print(duganam)
