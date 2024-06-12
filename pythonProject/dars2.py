99c1a6b5cce33b7663a23084ccefdfefd0147aa2
azizbek-312 <gullolaiskandarova36@gmail.com>
2024-06-12 16:31:36 +0500

    dars2

---
+++ pythonProject/dars2.py
@@ -0,0 +1,22 @@
+sonlar = list(range(11,101,2))
+sonlar_kubi = []
+for son in sonlar:
+    sonlar_kubi.append(son**3)
+print(sonlar)
+print(sonlar_kubi)
+
+kinolar=[]
+for n in range(5):
+    kinolar.append(input('Eng sevimli kinoni kiriting:'))
+print(kinolar)
+
+
+odamlar=[]
+for n in range(3):
+    odamlar.append(input(f"{n+1}- suhbat qilgan odamingiz kim edi:"))
+print(odamlar)
+
+ismlar = ['Ali', 'Vali', 'Hasan', 'Husan', 'Olim']
+for ism in ismlar:
+print(f"Salom {ism}")
+print("Kod 5 marta takrorlandi")
