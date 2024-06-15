d1d8fe045c1ae0a9ac26592e2b363194673e05c8
azizbek-312 <gullolaiskandarova36@gmail.com>
2024-06-15 15:10:25 +0500

    Funksiya

---
+++ Funksiya.py
@@ -0,0 +1,22 @@
+# def salom():
+#     print("Assalomu aleykum!")
+#
+# salom()
+# def salom_ber(name):
+#     print(f"Assalomu aleykum, hurmatli {name.title()}!")
+#
+# # salom_ber("Zilola")
+# def salom_ber():
+#     # "uhdufhidj"
+    # print("gygu")
+
+# # print(salom_ber.__doc__)
+# salom_ber("hasan")
+# salom_ber("olim")
+# def toliq_ism(ism,familiya):
+#     print(f"Ismi: {ism.title()}\n" {
+#           f"familiyasi: {familiya.title()}")
+# toliq_ism("Zilola", 'Iskandarova')
+# def yosh_hisobla(t_yil,j_yil):
+#     print(f"siz {j_yil-t_yil} yoshdasz")
+# yosh_hisobla(2009,2024)
