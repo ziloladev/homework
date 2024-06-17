3daa9d4490bfec0c87fab9b3f8435351ca50d204
azizbek-312 <gullolaiskandarova36@gmail.com>
2024-06-17 11:34:14 +0500

    dars

---
+++ dars-17, 18.py
@@ -0,0 +1,39 @@
+# def foydalanuvchi_info(ism, familiya, tyil, tjoy, email="", tel=None):
+#     foydalanuvchi = {'ism': ism,
+#                      'familiya': familiya,
+#                      'tyil': tyil,
+#                      'yoshi': tyil,
+#                      'tjoy': tjoy,
+#                      'email': email,
+#                      'telefon': tel }
+#
+# print("Mijoz haqida malumot.")
+# mijozlar = []
+# while True:
+#     print(f"\nQuydagi malumotlarni kiriting,")
+#     ism = input("Ismingiz kiriting: ")
+#     familiya = input("Familiyangizni kiriting: ")
+#     tyil = input("Tug'ilgan yilingizni kiriting: ")
+#     tjoy = input("Tug'ilgan joyingizni kiriting: ")
+#     email = input("Email manzilingizni kiriting: ")
+#     tel = input("Telefon raqamingizni kiriting: ")
+#     mijozlar.append(foydalanuvchi_info(ism, familiya, tyil, tjoy, email, tel))
+#     javob = input("Yana kiritasizmi? (yes/no):")
+#     if javob=='no':
+#         break
+#
+# def aylana_info(radius, pi=3.14159):
+#     aylana = {
+#         "radius": radius,
+#         "diametr": 2 * radius,
+#         "perimetr": 2 * radius * pi,
+#         "yuza": pi * radius ** 2,
+#     }
+#     return aylana
+
+
+
+
+
+
+
